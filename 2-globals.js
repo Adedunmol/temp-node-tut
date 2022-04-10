989ba482623b6273fde2115873dc5dc2b9fcbec5
Adedunmola Oyewale <oyewaleadedunmola@gmail.com>
2022-04-10 09:28:57 +0100

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,7 @@
+// GLOBALS
+
+//__dirname - path to current directory
+//__filename - path to current file
+//module - info about current module(file)
+//require - function to use modules(CommonJS)
+//process - info about env where the program is being executed
